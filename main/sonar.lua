342c92577d9264db8159376abf9b5af8e4ffd199
JustAPotota <justapotota.dev@gmail.com>
2023-04-24 13:07:50 -0500

    Remove debug prints

--- main/sonar.lua
+++ main/sonar.lua
@@ -7,7 +7,6 @@ local active_sonars = {}
 ---@param seconds_left vector4[]
 function M.new_sonar(position, color, seconds_left)
 	assert(#seconds_left <= 256)
-	print("seconds", #seconds_left)
 
 	local position = vmath.vector3(position.x, position.y, #active_sonars / 100)
 	local object_id = factory.create("#afactory", position)
@@ -17,9 +16,7 @@ function M.new_sonar(position, color, seconds_left)
 	go.set(model_url, "params.x", color.x)
 	go.set(model_url, "params.y", color.y)
 	go.set(model_url, "params.z", color.z)
-	print("color", color)
 
-	print("sec1", seconds_left[1])
 	for i, v in ipairs(seconds_left) do
 		go.set(model_url, "seconds_left", v, { index = i })
 	end
